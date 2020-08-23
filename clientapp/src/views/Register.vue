cdf7b24c04a918d1f8b027dee4b05999c616eeee
Stephen Bell <u3132293@uni.canberra.edu.au>
2020-08-23 14:46:44 +1000

    added basic form for register page

---
+++ clientapp/src/views/Register.vue
@@ -0,0 +1,16 @@
+﻿<template>
+    <section class="section">
+        <h1 class="title is-2">Register</h1>
+        <form>
+            <b-field label="Email">
+                <b-input placeholder="Enter your email">
+                </b-input>
+            </b-field>
+            <b-field label="Password">
+                <b-input type="password"
+                         placeholder="Enter a password">
+                </b-input>
+            </b-field>
+        </form>
+    </section>
+</template>
