2fcabd5b9a8803747f271559c01af41132ac5778
Stephen Bell <u3132293@uni.canberra.edu.au>
2020-08-24 23:30:34 +1000

    added data binding for form

--- clientapp/src/views/Register.vue
+++ clientapp/src/views/Register.vue
@@ -3,13 +3,13 @@
         <h1 class="title is-2">Register</h1>
         <form>
             <b-field label="Email">
-                <b-input placeholder="Enter your email">
-                </b-input>
+                <b-input placeholder="Enter your email"
+                         v-model="email" />
             </b-field>
             <b-field label="Password">
                 <b-input type="password"
-                         placeholder="Enter a password">
-                </b-input>
+                         placeholder="Enter a password"
+                         v-model="password" />
             </b-field>
             <b-button tag="input"
                       native-type="submit"
