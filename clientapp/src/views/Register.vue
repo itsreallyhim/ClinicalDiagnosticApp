bc3f2de244bd864ad3a7fd4713fb51a5c9926676
Stephen Bell <u3132293@uni.canberra.edu.au>
2020-08-25 16:35:53 +1000

    updated form to include client side validation

--- clientapp/src/views/Register.vue
+++ clientapp/src/views/Register.vue
@@ -1,20 +1,29 @@
 ﻿<template>
     <section class="section">
         <h1 class="title is-2">Register</h1>
-        <form>
-            <b-field label="Email">
-                <b-input placeholder="Enter your email"
-                         v-model="email" />
-            </b-field>
-            <b-field label="Password">
-                <b-input type="password"
-                         placeholder="Enter a password"
-                         v-model="password" />
-            </b-field>
+        <ValidationObserver ref="observer" v-slot="{ handleSubmit }">
+            <ValidationProvider rules="required|email" name="Email" v-slot="{ errors, valid }">
+                <b-field label="Email"
+                         :type="{ 'is-danger': errors[0], 'is-success': valid }"
+                         :message="errors">
+                    <b-input type="email" v-model="email" />
+                </b-field>
+            </ValidationProvider>
+            <ValidationProvider rules="required"
+                                vid="password"
+                                name="Password"
+                                v-slot="{ errors, valid }">
+                <b-field label="Password"
+                         :type="{ 'is-danger': errors[0], 'is-success': valid }"
+                         :message="errors">
+                    <b-input type="password"
+                             v-model="password" />
+                </b-field>
+            </ValidationProvider>
             <b-button tag="input"
                       native-type="submit"
                       value="Register" />
-        </form>
+        </ValidationObserver>
     </section>
 </template>
 
@@ -27,5 +36,9 @@
                 password: ''
             }
         },
+        methods: {
+            submit() {
+            },
+        }
     }
 </script>
