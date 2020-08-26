369bcfc8ab4327c9f980337e6e9fd1167db5c364
Stephen Bell <u3132293@uni.canberra.edu.au>
2020-08-26 10:09:14 +1000

    added fields to capture additional info about user

--- clientapp/src/views/Register.vue
+++ clientapp/src/views/Register.vue
@@ -2,6 +2,26 @@
     <section class="section">
         <h1 class="title is-2">Register</h1>
         <ValidationObserver ref="observer" v-slot="{ handleSubmit }">
+
+            <ValidationProvider rules="required" name="First Name" v-slot="{ errors, valid }">
+                <b-field label="First Name"
+                         :type="{ 'is-danger': errors[0], 'is-success': valid }"
+                         :message="errors">
+                    <b-input v-model="firstname" />
+                </b-field>
+            </ValidationProvider>
+            <ValidationProvider name="Middle Name" v-slot="{ errors, valid }">
+                <b-field label="Middle Name">
+                    <b-input v-model="middlename" />
+                </b-field>
+            </ValidationProvider>
+            <ValidationProvider rules="required" name="Last Name" v-slot="{ errors, valid }">
+                <b-field label="Last Name"
+                         :type="{ 'is-danger': errors[0], 'is-success': valid }"
+                         :message="errors">
+                    <b-input v-model="lastname" />
+                </b-field>
+            </ValidationProvider>
             <ValidationProvider rules="required|email" name="Email" v-slot="{ errors, valid }">
                 <b-field label="Email"
                          :type="{ 'is-danger': errors[0], 'is-success': valid }"
@@ -33,6 +53,9 @@
         name: 'Register',
         data() {
             return {
+                firstname: '',
+                middlename: '',
+                lastname: '',
                 email: '',
                 password: ''
             }
