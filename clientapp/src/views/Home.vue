b8eac2466268712afcbb6393303a4d3d1537f5d1
Stephen Bell <u3132293@uni.canberra.edu.au>
2020-08-26 13:29:49 +1000

    added introduction from template

--- clientapp/src/views/Home.vue
+++ clientapp/src/views/Home.vue
@@ -1,19 +1,71 @@
 <template>
-    <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+        <h3 class="title is-3">Introduction</h3>
+        <h5 class="subtitle is-5 has-text-weight-semibold">To complete this assessment</h5>
 
-    </div>
+        <p class="mb-3">
+            With the help of this clinical diagnostic assessment template, people with lower back and other musculo-skeletal pain
+            (plus employers, insurers, medical, fitness and manipulative therapy practitioners) will be able to gain a clearer
+            appreciation of the underlying cause of the pain and more accurately prescribe the mostappropriate rehab treatment -
+            one that focuses on treating the underlying cause of the problem.
+        </p>
+
+        <p class="mb-3">
+            <span class="has-text-weight-semibold">As an individual</span> you can work your way through the clinical diagnostic
+            assessment yourself - with the help of someone to take photos of you in various diagnostic postures.
+        </p>
+
+        <p class="mb-3">
+            We recommend you take the photos on a digital camera, smart phone or tablet. Crop the photos and then insert
+            them in to the Clinical Diagnostic Assessment template - in the places as illustrated in this document.
+        </p>
+
+        <p class="mb-3">
+            We recommend you take the photos on a digital camera, smart phone or tablet. Crop the photos and then insert
+            them in to the Clinical Diagnostic Assessment template - in the places as illustrated in this document.
+        </p>
+
+        <p class="mb-3">
+            In the workplace, fitness centre, surgery or manipulative therapy rooms all it needs is someone to take people
+            through the various assessments.
+        </p>
+
+        <p class="mb-3">
+            This document illustrates in detail the nature and scope of the assessment.
+        </p>
+
+        <p class="mb-3">
+            <span class="has-text-weight-semibold">Once you've signed up to complete the assessment</span> we'll send you
+            the Microsoft Word version of the template that you can complete on your own computer and email to us for evaluation and recommendations
+        </p>
+
+        <p class="mb-3">
+            The clinical diagnostic assessment comes with an <span class="has-text-weight-semibold">exercise prescription</span>
+            that treats the cause of the problem by loosening tight muscles, strengthening weak muscles and getting the skeleton back into better alignment.
+        </p>
+
+        <p class="mb-3">
+            The exercise prescription is the most effective prescription there is for the treatment of most personally-generated
+            musculo-skeletal dysfunctions. It's a prescription that people can administer themselves. In fact it's only they,
+            themselves who can administer it. That's why it's the cheapest rehab program there is.
+        </p>
+
+        <p class="mb-3">
+            Most joint and muscle pain is personally generated. In the case of lower back pain, tight muscles attached to the
+            pelvis have taken the pelvis and the bones above it out of alignment. Only on the rates of occasions is lower back
+            pain caused by a lack of rubbing, crunching, heating, vibrating, strapping, doping, surgery, stretching gadgets ...!
+        </p>
+
+        <p>
+            (If you're a medical practitioner or rehab therapist you'll be able to use the template to quickly gain an appreciation
+            of the strength, flexibility and skeletal alignment of your clients and provide them with an exercise prescription that they,
+            themselves can use to become pain free.)
+        </p>
+    </div> 
 </template>
 
 <script>
-    // @ is an alias to /src
-    import HelloWorld from '@/components/HelloWorld.vue'
-
     export default {
-        name: 'Home',
-        components: {
-            HelloWorld
-        }
+        name: 'Home'
     }
 </script>
