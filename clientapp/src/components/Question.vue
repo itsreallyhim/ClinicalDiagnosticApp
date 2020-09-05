585b6e9628e0329ebe282c3f763b40914d029dfa
Mathias Everson <mathias@mathiaseverson.com>
2020-09-05 18:18:25 +1000

    Question and Range components with demo assessment

---
+++ clientapp/src/components/Question.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>
+      {{ question.title }}
+    </h2>
+    <component :is="question.type" v-model="question.value" />
+  </div>
+</template>
+
+<script>
+import Range from "../components/Range";
+export default {
+  components: { Range },
+
+  props: {
+    question: {
+      Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ clientapp/src/views/Assessment.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Question
+      v-model="item.value"
+      v-for="(item, index) in items"
+      :key="index"
+      :question="item"
+    />
+
+    {{ items }}
+
+    Sum{{ pageTotal }}
+  </div>
+</template>
+
+<script>
+import Question from "../components/Question";
+
+export default {
+  components: { Question },
+  data() {
+    return {
+      items: [
+        {
+          type: "Range",
+          value: null,
+          title: "Question 1",
+          description: "Description of Question 1.",
+        },
+        {
+          type: "Range",
+          value: null,
+          title: "Question 2",
+          description: "Description of Question 2",
+        },
+      ],
+    };
+  },
+  computed: {
+    pageTotal() {
+      return this.items.reduce((sum, current) => (sum += current.value), 0);
+    },
+  },
+};
+</script>
+
+<style></style>
