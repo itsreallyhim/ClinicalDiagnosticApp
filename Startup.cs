be858ce06fcdc7acde2eedfa40d297a2ddcfca7b
Mathias Everson <mathias@mathiaseverson.com>
2020-09-19 20:38:50 +1000

    Update dependncies and startup

--- Startup.cs
+++ Startup.cs
@@ -1,23 +1,94 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using ClinicalDiagnosticApp.Data;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+//using VueCliMiddleware;
+
+//namespace ClinicalDiagnosticApp
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
+
+//        public IConfiguration Configuration { get; }
+
+//        // This method gets called by the runtime. Use this method to add services to the container.
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            var connection = Configuration.GetConnectionString("Database");
+
+
+//            services.AddDbContext<MillerHealthContext>(op => op.UseSqlServer(connection), ServiceLifetime.Transient);
+//            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+//            services.AddControllers();
+//            services.AddSpaStaticFiles(configuration =>
+//            {
+//                configuration.RootPath = "ClientApp";
+//            });
+
+
+//        }
+
+//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//            }
+
+//            app.UseRouting();
+//            app.UseSpaStaticFiles();
+//            app.UseAuthorization();
+
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapControllers();
+//            });
+
+//            app.UseSpa(spa =>
+//            {
+//                if (env.IsDevelopment())
+//                    spa.Options.SourcePath = "ClientApp";
+//                else
+//                    spa.Options.SourcePath = "dist";
+
+//                if (env.IsDevelopment())
+//                {
+//                    spa.UseVueCli(npmScript: "serve", port: 8081);
+//                }
+
+//            });
+//        }
+//    }
+//}
 using ClinicalDiagnosticApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using VueCliMiddleware;
 
 namespace ClinicalDiagnosticApp
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -30,47 +101,36 @@ namespace ClinicalDiagnosticApp
         {
             var connection = Configuration.GetConnectionString("Database");
 
+            services.AddDbContext<MillerHealthContext>(op => op.UseSqlServer(connection), ServiceLifetime.Transient);
 
-            services.AddDbContext<MillerHealthContext>(op =>  op.UseSqlServer(connection), ServiceLifetime.Transient);
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            // NOTE: PRODUCTION Ensure this is the same path that is specified in your webpack output
+            services.AddSpaStaticFiles(opt => opt.RootPath = "ClientApp/dist");
             services.AddControllers();
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp";
-            });
-
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // optional base path for IIS virtual app/directory
+            app.UsePathBase("/optionalpath");
 
-            app.UseRouting();
+            // PRODUCTION uses webpack static files
             app.UseSpaStaticFiles();
-            app.UseAuthorization();
 
+            // Routing
+            app.UseRouting();
+            //app.UserAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
 
-            app.UseSpa(spa =>
-            {
-                if (env.IsDevelopment())
-                    spa.Options.SourcePath = "ClientApp";
-                else
-                    spa.Options.SourcePath = "dist";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseVueCli(npmScript: "serve");
-                }
-
+                endpoints.MapToVueCliProxy(
+                    "{*path}",
+                    new SpaOptions { SourcePath = "ClientApp" },
+                    npmScript: (System.Diagnostics.Debugger.IsAttached) ? "serve" : null,
+                    regex: "Compiled successfully",
+                    forceKill: true
+                    );
             });
         }
     }
