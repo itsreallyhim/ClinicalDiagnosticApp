c1e187614ddf7248e83c732d15a425c8851e6f50
Stephen Bell <u3132293@uni.canberra.edu.au>
2020-09-22 20:18:48 +1000

    user service, added function to register account

---
+++ clientapp/src/services/user.service.js
@@ -0,0 +1,15 @@
+﻿import config from 'config';
+const axios = require('axios');
+
+export const userService = {
+	register
+};
+
+async function register(user) {
+    try {
+        await axios.post(`${config.apiUrl}/users/register`, user)
+    }
+    catch (error) {
+        return Promise.reject(error.response.data.errors)
+    }
+}
