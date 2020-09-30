011dd652a2685bc949306f733ffd06825f43bd08
Stephen Bell <u3132293@uni.canberra.edu.au>
2020-09-30 20:34:11 +1000

    added loading effect and changed size of box

--- clientapp/src/views/Auth.vue
+++ clientapp/src/views/Auth.vue
@@ -1,35 +1,46 @@
 <template>
-    <section class="section">
-        <div class="container">
-            <div class="columns is-centered">
-                <div class="column is-4">
-                    <div class="box">
-                        <h1 class="title">
-                            {{ title }}
-                        </h1>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-5">
+          <div class="box">
+            <h1 class="title">
+              {{ title }}
+            </h1>
 
-                        <router-view></router-view>
+            <router-view  @clicked="handleSubmit"></router-view>
 
-                        <p class="is-size-7">
-                            Use of this application is in accordance to our Privacy Policy and Terms
-                            of Use.
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <p class="is-size-7">
+              Use of this application is in accordance to our Privacy Policy and Terms
+              of Use.
+            </p>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+    <b-loading :is-full-page="false" v-model="isLoading"></b-loading>
+  </section>
 </template>
 
 <script>
-    export default {
-        name: "Auth",
-        computed: {
-            title() {
-                return this.$route.name || "Auth";
-            },
-        },
-    };
+  export default {
+    name: "Auth",
+    data() {
+      return {
+        isLoading: false
+      }
+    },
+    computed: {
+      title() {
+        return this.$route.name || "Auth";
+      },
+    },
+    methods: {
+      handleSubmit(value) {
+        this.isLoading = value;
+      }
+    }
+  };
 </script>
 
 <style></style>
